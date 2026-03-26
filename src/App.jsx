d5dadb71e64b4c6f85414de12d35faae3f5cf832
semartinl <sermarled10@gmail.com>
2026-03-26 11:46:25 +0100

    Changing import src

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import SideBar from "./components/Sidebar/SideBar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Headers from "./pages/Headers";
 import Dropdowns from "./pages/Dropdowns";
 
